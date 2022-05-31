de795455055c53029e8e4ade3207989d710066c2
Narayan Dhakal <narayandhakal443@gmail.com>
2022-05-31 20:37:11 +0545

    class work

---
+++ Classwork/js/external.js
@@ -0,0 +1,10 @@
+
+
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListiner('click', function () {
+    alert('click');
+})
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListiner('mouseover', function () {
+    alert('click');
+})
