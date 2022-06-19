2512e880a987aa6b3e80ce1efc19c5b188f9ecf4
Narayan Dhakal <narayandhakal443@gmail.com>
2022-06-19 17:03:27 +0545

    lab6.1

--- Assignments/Assignment 6/script.js
+++ Assignments/Assignment 6/script.js
@@ -1,12 +1,7 @@
 function reset() {
     location.reload(true);
 };
-
 function dragndrop() {
-    
-
-
-
     let position_x;
     let position_y;
     let select;
@@ -14,26 +9,20 @@ function dragndrop() {
         select = e.target;
         position_x = e.offsetX === undefined ? e.layerX : e.offsetX;
         position_y = e.offsetY === undefined ? e.layerY : e.offsetY;
-        select.style.zIndex = 100;
+        select.style.zIndex = 5;
+        select.style.
     }
     document.querySelector('body').addEventListener('dragstart', drag, false);
-
-
     function move(e) {
         console.log(e.preventDefault())
         e.preventDefault();
     }
     document.querySelector('body').addEventListener('dragover', move, false);
-
-
     function drop(e) {
         e.preventDefault();
         select.style.left = e.pageX - position_x + 'px';
         select.style.top = e.pageY - position_y + 'px';
     }
-
-
     document.querySelector('body').addEventListener('drop', drop, false);
-
 }
 dragndrop();
---
+++ Classwork/js/dynamiccontent/script.js
@@ -0,0 +1,15 @@
+const add = document.querySelector('#addbtn');
+const remove = document.querySelector('#remove');
+const getcontainer = document.querySelector('#container');
+const boxs = "<div class='box'></div>";
+const getcontainerdiv = document.querySelector('#container div');
+
+
+add.addEventListener("click", function () {
+    getcontainer.innerHTML += boxs;
+    
+})
+remove.addEventListener("click", function () {
+    console.log(getcontainerdiv);
+
+})
